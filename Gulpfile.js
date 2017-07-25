79db218fc1fcd58f60a19bc5d19b5e492f4ebff7
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2017-07-25 13:38:21 +0200

    Clean up Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,10 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const ghPages = require('gulp-gh-pages');
 
-gulp.task('deploy', () => {
-	gulp.src(['index.html']).pipe(gulp.dest('dist/'));
-	return gulp.src('./dist/**/*').pipe(ghPages());
-});
+gulp.task('deploy', () => gulp.src('./dist/**/*').pipe(ghPages()));
 
 gulp.task('sass', () => gulp.src('./app/sass/main.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./dist/')));
 
