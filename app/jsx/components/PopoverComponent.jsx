0f85cff42dfb82a07ab806f5c24dea7b9284e576
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2017-07-27 09:34:33 +0200

    Make close icon in popover 2x

--- app/jsx/components/PopoverComponent.jsx
+++ app/jsx/components/PopoverComponent.jsx
@@ -11,7 +11,7 @@ class PaymentTemplate extends React.Component {
 			<div className="panel panel-default info-popover">
 				<div className="panel-heading">
 					{this.props.title}
-					<i className="pull-right fa fa-times pointer" onClick={this.props.close}></i>
+					<i className="pull-right fa fa-2x fa-times pointer" onClick={this.props.close}></i>
 				</div>
 				<div className="panel-body">
 					{this.props.children}
