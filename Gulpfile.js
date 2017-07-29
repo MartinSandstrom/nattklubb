b9eb3dca1eec062ed920f9074f232410568c3cf3
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2017-07-29 14:57:27 +0200

    Update 2017-07-29T12:57:25.046Z

--- Gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const ghPages = require('gulp-gh-pages');
-
-gulp.task('deploy', () => gulp.src('./dist/**/*').pipe(ghPages({force: true})));
-
-gulp.task('sass', () => gulp.src('./app/sass/main.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./dist/')));
-
-gulp.task('sass:watch', () => gulp.watch('./app/sass/**/*.scss', ['sass']));
